c9eb1dd4651b5f2126e41b378313dcff95a95c9e
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-18 16:33:30 +0200

    added bonus from yesterday exercise - completely ended exercise day 17-05-2023

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -16,11 +16,42 @@
         <button class="btn btn-primary">Aggiorna</button>
     </a>
 
-    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-        @csrf
-        @method('DELETE');
-            <button class="btn btn-danger" type="submit">Elimina</button>
-    </form>
+    <!-- Button trigger modal -->
+    <div>
+
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Elimina
+        </button>
+    </div>
+        
+  <!-- Modal -->
+ 
+
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">!!! LEGGI ATTENTAMENTE !!!</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            <div class="modal-body">
+                Procedendo con l'eliminazione, si perderenno in modo definitivo tutti i dati relativi a questo elemento.<br>
+                Sei sicuro di voler procedere?
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                            <button class="btn btn-danger" type="submit">Elimina</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+  
+    
 </div>
     
 @endsection
