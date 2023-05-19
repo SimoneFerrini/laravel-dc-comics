b75484a16231eef58a3e12306b79de4b65217591
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-19 08:01:59 +0200

    ended create validate with bonus in italian

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,15 +5,6 @@
 
     <div class="container create-section">
     
-        @if ($errors->any())
-            <ul class="text-black">
-              @foreach ($errors->all() as $singleError)
-                  <li>
-                    {{$singleError}}
-                  </li>
-              @endforeach
-            </ul>
-        @endif
 
 
         <form action="{{route('comics.store')}}" method="POST">
@@ -21,37 +12,73 @@
     
           <div class="mb-3">
             <label class="my-label" for="title">Titolo</label>
-            <input type="text" id="title" name="title" required>
+            <input class="@error('title') is-invalid @enderror" type="text" id="title" name="title" required value="{{old('title')}}">
+            @error('title')
+              <div class="invalid-feedback">
+                Il titolo non è stato inserito correttamente - {{$message}}
+              </div>
+            @enderror
+
           </div>
     
           <div class="mb-3">
             <label class="my-label" for="description">Descrizione</label>
-            <textarea id="description" name="description" required></textarea>
+            <textarea class="@error('description') is-invalid @enderror" id="description" name="description" required>{{old('description')}}</textarea>
+            @error('description')
+              <div class="invalid-feedback">
+                La descrizione non è stata inserita correttamente - {{$message}}
+              </div>
+            @enderror
           </div>
     
           <div class="mb-3">
             <label class="my-label" for="thumb">Thumb</label>
-            <input type="text" id="thumb" name="thumb" required>
+            <input class="@error('thumb') is-invalid @enderror" type="text" id="thumb" name="thumb" required value="{{old('thumb')}}">
+            @error('thumb')
+              <div class="invalid-feedback">
+                Il percorso dell'immagine non è stato inserito correttamente - {{$message}}
+              </div>
+            @enderror
           </div>
     
           <div class="mb-3">
             <label class="my-label" for="price">Prezzo:</label>
-            <input type="text" id="price" name="price" required>
+            <input class="@error('price') is-invalid @enderror" type="text" id="price" name="price" required value="{{old('price')}}">
+            @error('price')
+              <div class="invalid-feedback">
+                Il prezzo non è stato inserito correttamente - {{$message}}
+              </div>
+            @enderror
           </div>
     
           <div class="mb-3">
             <label class="my-label" for="series">Serie</label>
-            <input type="text" id="series" name="series" required>
+            <input class="@error('series') is-invalid @enderror" type="text" id="series" name="series" required value="{{old('series')}}">
+            @error('series')
+              <div class="invalid-feedback">
+                La serie non è stata inserita correttamente - {{$message}}
+              </div>
+            @enderror
           </div>
     
           <div class="mb-3">
             <label class="my-label" for="sale_date">Data uscita</label>
-            <input type="text" id="sale_date" name="sale_date" required>
+            <input class="@error('sale_date') is-invalid @enderror" type="text" id="sale_date" name="sale_date" required value="{{old('sale_date')}}">
+            @error('sale_date')
+              <div class="invalid-feedback">
+                La data non è stata inserita correttamente - {{$message}}
+              </div>
+            @enderror
           </div>
     
           <div class="mb-3">
               <label class="my-label" for="type">Genere</label>
-              <input type="text" id="type" name="type" required>
+              <input class="@error('type') is-invalid @enderror" type="text" id="type" name="type" required value="{{old('type')}}">
+              @error('type')
+              <div class="invalid-feedback">
+                Il tipo non è stato inserito correttamente - {{$message}}
+              </div>
+            @enderror
             </div>
     
           <button class="btn btn-primary my-btn" type="submit">Add</button>
